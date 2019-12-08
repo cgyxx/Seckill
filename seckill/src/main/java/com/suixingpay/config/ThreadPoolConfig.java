32cb97f44a21e05e533d5b8d685a432df0cbb51e
liushuai <1154722967@qq.com>
2019-12-08 17:16:01 +0800

    添加发邮件接口 修改线程池的大小

--- seckill/src/main/java/com/suixingpay/config/ThreadPoolConfig.java
+++ seckill/src/main/java/com/suixingpay/config/ThreadPoolConfig.java
@@ -23,11 +23,11 @@ public class ThreadPoolConfig {
     public TaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         // 设置核心线程数
-        executor.setCorePoolSize(2);
+        executor.setCorePoolSize(100);
         // 设置最大线程数
-        executor.setMaxPoolSize(2);
+        executor.setMaxPoolSize(500);
         // 设置队列容量
-        executor.setQueueCapacity(2);
+        executor.setQueueCapacity(1000);
         // 设置线程活跃时间（秒）
         executor.setKeepAliveSeconds(300);
         // 设置默认线程名称

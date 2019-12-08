7da9c270791b99d0a72e3c545e092c68fcc86060
liushuai <1154722967@qq.com>
2019-12-08 18:22:00 +0800

    添加活动cotroller

--- seckill/src/main/java/com/suixingpay/controller/ActivityController.java
+++ seckill/src/main/java/com/suixingpay/controller/ActivityController.java
@@ -8,10 +8,15 @@
  */
 package com.suixingpay.controller;
 
+import com.suixingpay.entity.Activity;
 import com.suixingpay.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @description:
@@ -26,9 +31,36 @@ public class ActivityController {
     @Autowired
     private ActivityService activityService;
 
-    @RequestMapping("test")
-    public Integer Test(Integer id){
+    @RequestMapping("add")
+    public Integer add(Activity activity){
+        return activityService.insertSelective(activity);
+    }
+    @RequestMapping("delete")
+    public Integer delete(Integer id){
         return activityService.deleteByPrimaryKey(id);
     }
+    @RequestMapping("findById")
+    public Activity findById(Integer id){
+        return activityService.selectById(id);
+    }
+    @RequestMapping("findAll")
+    public Map findAll(){
+        Map<String,List<Activity>> map = new HashMap<>();
+        map.put("activitys",activityService.selectAll());
+        return map;
+    }
+    @RequestMapping("findByCity")
+    public Map findAll(String city){
+        Map<String,List<Activity>> map = new HashMap<>();
+        map.put("activitys",activityService.selectByCity(city));
+        return map;
+    }
+    @RequestMapping("findValid")
+    public Map findValid(){
+        Map<String,List<Activity>> map = new HashMap<>();
+        map.put("activitys",activityService.selectValidActivity());
+        return map;
+    }
+
 
 }

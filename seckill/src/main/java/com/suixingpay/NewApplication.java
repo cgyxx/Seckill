2a3b5fc52a75d6c645b18a80c11ad07a49109166
Aaaina <18845074110@163.com>
2019-12-08 13:19:29 +0800

    1

---
+++ seckill/src/main/java/com/suixingpay/NewApplication.java
@@ -0,0 +1,15 @@
+package com.suixingpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: weishangqi<wei_sq @ suixingpay.com>
+ * @date:2019/12/7 22:07
+ */
+@SpringBootApplication
+public class NewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NewApplication.class,args);   }
+}

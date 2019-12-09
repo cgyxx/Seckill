a1c26567fe84489e423bb070ac8be58445ad2424
Aaaina <18845074110@163.com>
2019-12-09 10:38:12 +0800

    沉默用户查询服务层

--- seckill/src/main/java/com/suixingpay/service/SilentuserService.java
+++ seckill/src/main/java/com/suixingpay/service/SilentuserService.java
@@ -9,6 +9,10 @@
 package com.suixingpay.service;
 
 
+import com.suixingpay.entity.Silentuser;
+
+import java.util.List;
+
 /**
  * @description:
  * @author: jiangyuanchu<jiang_yc@suixingpay.com>
@@ -16,4 +20,11 @@ package com.suixingpay.service;
  * @version: V1.0
  */
 public interface SilentuserService {
+    Silentuser selectById(Integer id);//根据id查询沉默用户的信息
+    List<Silentuser> selectAllSilentuser();//查询所有沉默用户的所有信息
+    Silentuser selectSilentUserBySlaesid(Integer slaesid);//根据所属商户id查询沉默用户
+    Silentuser selectSilentUserById(Integer id);//根据查询沉默用户
+    Integer selectSilentUserIsSilentuer();//查询状态为1的沉默用户的数量
+    Integer selectSilentUserActiveuer();//查询状态为2的沉默用户的数量
+    Integer selectSilentUserIsDeaduer();//查询状态为3的沉默用户的数量
 }
